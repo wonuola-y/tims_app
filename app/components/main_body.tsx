3f110f911272b31ed455326a8bbd61ed4f56bde2
Wonuola <wonuolalonge@gmail.com>
2025-02-21 00:11:37 +0100

    style fix

--- app/components/main_body.tsx
+++ app/components/main_body.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const main_body = () => {
   return (
       <div className='md:h-screen  my-20 mb-20 flex flex-col  justify-center items-center '>
-          <h1 className="font-bodoni_modaitalic text-3xl text-gray-400 mt-20 md:mt-0 italic" >Let me guess</h1>
+          <h1 className="font-bodoni_modaitalic text-3xl text-gray-400 mt-24 md:mt-0 italic" >Let me guess</h1>
           <h1 className='uppercase text-3xl md:text-5xl text-center my-5 w-full tracking-wide md:tracking-normal md:w-2/3 text-black'>YOU’RE Ready to Skyrocket Your Social Media Influence</h1>
           <p className="border-[1px] border-gray-400 w-[20%] my-5"></p>
          <div className='text-center p-5 pt-0 md:p-5 text-black'>
